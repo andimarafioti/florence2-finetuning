d8c4d39e81f3e802494711c390d01ea9794140ab
Andres Marafioti <andimarafioti@gmail.com>
2024-06-20 13:24:08 +0000

    evaluate and train

---
+++ evaluate.py
@@ -0,0 +1,97 @@
+import torch.multiprocessing as mp
+from transformers import AutoProcessor, AutoModelForCausalLM
+from datasets import load_dataset
+import Levenshtein
+import logging
+from tqdm import tqdm
+from data import DocVQADataset
+import torch
+from torch.utils.data import DataLoader, Subset
+import random
+
+from metrics import average_normalized_levenshtein_similarity 
+
+# Set the multiprocessing start method to 'spawn'
+# mp.set_start_method('spawn', force=True)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load the dataset
+data = load_dataset("HuggingFaceM4/DocumentVQA")
+
+# Load the model and processor
+model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-base", trust_remote_code=True).to(device)
+processor = AutoProcessor.from_pretrained("microsoft/Florence-2-base", trust_remote_code=True)
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Function to run the model on an example
+def run_example(task_prompt, text_input, image):
+    prompt = task_prompt + text_input
+    
+    # Ensure the image is in RGB mode
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    
+    inputs = processor(text=prompt, images=image, return_tensors="pt").to(device)
+    generated_ids = model.generate(
+        input_ids=inputs["input_ids"],
+        pixel_values=inputs["pixel_values"],
+        max_new_tokens=1024,
+        num_beams=3
+    )
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+    parsed_answer = processor.post_process_generation(generated_text, task=task_prompt, image_size=(image.width, image.height))
+    return parsed_answer
+
+
+def collate_fn(batch):
+    questions, answers, images = zip(*batch)
+    inputs = processor(text=list(questions), images=list(images), return_tensors="pt", padding=True).to(device)
+    return inputs, answers
+
+# Create DataLoader
+batch_size = 16  # Adjust the batch size based on your GPU memory
+num_workers = 0  # Number of worker processes to use for data loading
+prefetch_factor = None  # Number of batches to prefetch
+
+test_dataset = DocVQADataset(data['validation'])
+
+# Create a subset of the dataset
+subset_size = int(0.01 * len(test_dataset))  # 10% of the dataset
+indices = random.sample(range(len(test_dataset)), subset_size)
+subset_dataset = Subset(test_dataset, indices)
+
+test_loader = DataLoader(subset_dataset, batch_size=batch_size, collate_fn=collate_fn, num_workers=num_workers, prefetch_factor=prefetch_factor)
+
+def run_batch(inputs):
+    generated_ids = model.generate(
+        input_ids=inputs["input_ids"],
+        pixel_values=inputs["pixel_values"],
+        max_new_tokens=1024,
+        num_beams=3
+    )
+    generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=False)
+    return generated_texts
+
+def evaluate_model(test_loader):
+    task_prompt = "<VQA>"
+    predicted_answers = []
+    ground_truth = []
+
+    for inputs, batch_answers in tqdm(test_loader, desc="Evaluating"):
+        generated_texts = run_batch(inputs)
+        
+        for generated_text, answers in zip(generated_texts, batch_answers):
+            parsed_answer = processor.post_process_generation(generated_text, task=task_prompt, image_size=(inputs["pixel_values"].shape[-2], inputs["pixel_values"].shape[-1]))
+            predicted_answers.append(parsed_answer[task_prompt])
+            ground_truth.append(answers)
+
+    avg_levenshtein_similarity = average_normalized_levenshtein_similarity(ground_truth, predicted_answers)
+    return answers, avg_levenshtein_similarity
+
+# Run the evaluation
+answers, average_similarity = evaluate_model(test_loader)
+print(f"Average Normalized Levenshtein Similarity: {average_similarity}")
---
+++ train.py
@@ -0,0 +1,114 @@
+from datasets import load_dataset
+import torch
+from torch.utils.data import DataLoader, Subset, random_split
+from transformers import AutoProcessor, AutoModelForCausalLM, AdamW, get_scheduler
+from tqdm import tqdm
+import random
+import os
+
+from data import DocVQADataset
+
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load the dataset
+data = load_dataset("HuggingFaceM4/DocumentVQA")
+
+# Load the model and processor
+model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-base", trust_remote_code=True).to(device)
+processor = AutoProcessor.from_pretrained("microsoft/Florence-2-base", trust_remote_code=True)
+
+def collate_fn(batch):
+    questions, answers, images = zip(*batch)
+    inputs = processor(text=list(questions), images=list(images), return_tensors="pt", padding=True).to(device)
+    return inputs, answers
+
+# Create datasets
+train_dataset = DocVQADataset(data['train'])
+val_dataset = DocVQADataset(data['validation'])
+
+# Create DataLoader
+batch_size = 8
+num_workers = 0
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, collate_fn=collate_fn, num_workers=num_workers, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, collate_fn=collate_fn, num_workers=num_workers)
+
+def train_model(train_loader, val_loader, model, processor, epochs=3, lr=5e-5):
+    optimizer = AdamW(model.parameters(), lr=lr)
+    num_training_steps = epochs * len(train_loader)
+    lr_scheduler = get_scheduler(
+        name="linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=num_training_steps,
+    )
+
+    for epoch in range(epochs):
+        # Training phase
+        model.train()
+        train_loss = 0
+        i = -1
+        for batch in tqdm(train_loader, desc=f"Training Epoch {epoch + 1}/{epochs}"):
+            i += 1
+            inputs, answers = batch
+
+            input_ids = inputs["input_ids"]
+            pixel_values = inputs["pixel_values"]
+            labels = processor.tokenizer(text=answers, return_tensors="pt", padding=True, return_token_type_ids=False).input_ids.to(device)
+
+            outputs = model(input_ids=input_ids, pixel_values=pixel_values, labels=labels)
+            loss = outputs.loss
+
+            if i%200 == 0:
+                print(loss)
+
+                generated_ids = model.generate(
+                    input_ids=inputs["input_ids"],
+                    pixel_values=inputs["pixel_values"],
+                    max_new_tokens=1024,
+                    num_beams=3
+                )
+                generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=False)
+
+                for generated_text, answer in zip(generated_texts, answers):
+                    parsed_answer = processor.post_process_generation(generated_text, task="<DocVQA>", image_size=(inputs["pixel_values"].shape[-2], inputs["pixel_values"].shape[-1]))
+                    print("GT:", answer)
+                    print("Pred:", parsed_answer["<DocVQA>"])
+
+            loss.backward()
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+
+            train_loss += loss.item()
+
+        avg_train_loss = train_loss / len(train_loader)
+        print(f"Average Training Loss: {avg_train_loss}")
+
+        # Validation phase
+        model.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for batch in tqdm(val_loader, desc=f"Validation Epoch {epoch + 1}/{epochs}"):
+                inputs, answers = batch
+
+                input_ids = inputs["input_ids"]
+                pixel_values = inputs["pixel_values"]
+                labels = processor.tokenizer(text=answers, return_tensors="pt", padding=True, return_token_type_ids=False).input_ids.to(device)
+
+                outputs = model(input_ids=input_ids, pixel_values=pixel_values, labels=labels)
+                loss = outputs.loss
+
+                val_loss += loss.item()
+
+        avg_val_loss = val_loss / len(val_loader)
+        print(f"Average Validation Loss: {avg_val_loss}")
+
+        # Save model checkpoint
+        output_dir = f"./model_checkpoints/epoch_{epoch+1}"
+        os.makedirs(output_dir, exist_ok=True)
+        model.save_pretrained(output_dir)
+        processor.save_pretrained(output_dir)
+
+train_model(train_loader, val_loader, model, processor, epochs=3)
