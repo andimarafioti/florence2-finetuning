72b21eb3efbe9c89c2fca6b7d80ba5e9244b9a43
Andres Marafioti <andimarafioti@gmail.com>
2024-06-21 09:29:03 +0000

    fixing the metrics

--- metrics.py
+++ metrics.py
@@ -6,18 +6,23 @@ import Levenshtein
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 # Levenshtein similarity functions
 def normalized_levenshtein(s1, s2):
     len_s1, len_s2 = len(s1), len(s2)
     distance = Levenshtein.distance(s1, s2)
     return distance / max(len_s1, len_s2)
 
+
 def similarity_score(a_ij, o_q_i, tau=0.5):
     nl = normalized_levenshtein(a_ij, o_q_i)
     return 1 - nl if nl < tau else 0
 
+
 def average_normalized_levenshtein_similarity(ground_truth, predicted_answers):
-    assert len(ground_truth) == len(predicted_answers), "Length of ground_truth and predicted_answers must match."
+    assert len(ground_truth) == len(
+        predicted_answers
+    ), "Length of ground_truth and predicted_answers must match."
 
     N = len(ground_truth)
     total_score = 0
@@ -29,8 +34,7 @@ def average_normalized_levenshtein_similarity(ground_truth, predicted_answers):
             logger.warning("Skipped an empty prediction.")
             max_score = 0
         else:
-            max_score = max(similarity_score(a_ij, o_q_i) for a_ij in a_i)
-
+            max_score = similarity_score(a_i, o_q_i)
         total_score += max_score
 
     return total_score / N
